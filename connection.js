13fa9cb6ee345dd17e227774b3d91ac8ecc8df63
jandirp24 <apalmacastellon@gmail.com>
2025-03-24 10:18:55 -0600

    .env

--- connection.js
+++ connection.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
 
 let connection;
 
 async function conectarBD() {
   if (!connection) {
     connection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'Appsweb2022+',
-      database: 'libros_db'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
     });
   }
   return connection;
